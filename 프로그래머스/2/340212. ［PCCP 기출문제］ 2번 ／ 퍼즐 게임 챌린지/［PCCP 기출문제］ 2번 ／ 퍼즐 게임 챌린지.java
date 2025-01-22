2d743db782e70e45dbd91951f34fb294ee896eb3
Layla Oh <crispylemon7120@gmail.com>
2025-01-22 17:52:27 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 76.49 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,57 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] diffs, int[] times, long limit) {
+        Puzzle puzzle = new Puzzle(diffs, times, limit);
+        int left = Arrays.stream(diffs).min().getAsInt();
+        int right = Arrays.stream(diffs).max().getAsInt();
+        int mid = (int) (left + right) / 2;
+        long total_time = limit + 1;
+        
+        while (left < mid){
+            total_time = 0;
+            for(int i = 0; i < diffs.length; i++){
+                total_time += puzzle.getTime(mid, i);
+            }
+            if(total_time >= limit){
+                left = mid;
+            }
+            else{
+                right = mid;
+            }
+            mid = (int) (left + right) / 2;
+        }
+        
+        
+        total_time = 0;
+        for(int i = 0; i < diffs.length; i++){
+            total_time += puzzle.getTime(left, i);
+        }
+        if (total_time <= limit)
+            return left;
+        return right;
+    }
+
+    public static class Puzzle{
+        int[] diffs;
+        int[] times;
+        long limit;
+
+        Puzzle(int[] diffs, int[] times, long limit) {
+            this.diffs = diffs;
+            this.times = times;
+            this.limit = limit;
+        }
+
+        public int getTime(int level, int idx){
+            if (this.diffs[idx] <= level){
+                return times[idx];
+            }
+            else{
+                return (this.diffs[idx] - level) * (times[idx] + times[idx - 1]) + times[idx];
+            }
+        }
+    }
+
+
+}
