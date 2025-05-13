bf7f736702923fc0e699f5a663d757bfd0ead02c
Layla Oh <crispylemon7120@gmail.com>
2025-05-13 10:30:09 +0900

    [Bronze V] Title: 오늘 날짜, Time: 168 ms, Memory: 17252 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
