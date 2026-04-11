687842647643a56a8c8d96ab6cda5193a880b024
Layla Oh <crispylemon7120@gmail.com>
2026-04-11 11:28:13 +0900

    [Gold IV] Title: 연구소, Time: 968 ms, Memory: 300604 KB -BaekjoonHub

---
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -0,0 +1,110 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+	static int N, M, answer = 0, space;
+
+	static int[] dr = {0, 0, -1, 1};
+	static int[] dc = {1, -1, 0, 0};
+	static ArrayList<int[]> virus;
+	
+	static int[][] board;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		board = new int[N][M];
+		space = 0;
+	
+		virus = new ArrayList<>();
+		
+		for(int r=0; r<N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for(int c=0; c<M; c++){
+				board[r][c] = Integer.parseInt(st.nextToken());
+				if(board[r][c] == 2) {
+					virus.add(new int[]{r, c});
+				}
+				if(board[r][c] == 0) {
+					space++;
+				}
+			}
+		}
+		
+		DFS(0);
+		System.out.println(answer);
+	}
+	
+	public static int[][] spread(int[][] newboard){
+		Deque<int[]> q = new ArrayDeque<>();
+		for(int i=0; i<virus.size(); i++) {
+			q.add(virus.get(i));
+		}
+		
+		while(!q.isEmpty()) {
+			int[] v = q.poll();
+			
+			for(int i=0; i<4; i++) {
+				int nr = v[0] + dr[i];
+				int nc = v[1] + dc[i];
+				
+				if(check(nr, nc) && newboard[nr][nc] == 0) {
+					newboard[nr][nc] = 2;
+					q.add(new int[] {nr, nc});
+				}
+			}
+		}
+		
+		return newboard;
+		
+	}
+	public static int safeplace() {
+		int[][] newboard = new int[N][M];
+		for(int i=0; i<N; i++) {
+			newboard[i] = board[i].clone();
+		}
+		
+		newboard = spread(newboard);
+		
+		int safespace = 0;
+		
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++){
+				if(newboard[r][c] == 0) {
+					safespace++;
+				}
+			}
+		}
+		
+		return safespace;
+	}
+
+	
+	public static void DFS(int count) {
+		if(count == 3) {
+			int safespace = safeplace();
+			if(safespace > 0)
+				answer = Math.max(answer, safeplace());
+			return;
+		}
+		
+		for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++){
+				if(board[r][c] == 0) {
+					board[r][c] = 1;
+					DFS(count + 1);
+					board[r][c] = 0;
+				}
+			}
+		}
+	}
+	
+	public static boolean check(int r, int c) {
+		return 0 <= r && r < N && 0 <= c && c < M;
+	}
+}
