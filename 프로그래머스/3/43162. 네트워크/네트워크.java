12a8c700ba1b3300dd6555008936020578ec4cf5
Hyunjung Oh <crispylemon7120@gmail.com>
2026-07-21 22:44:23 +0900

    [level 3] Title: 네트워크, Time: 0.14 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,52 +1,15 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] computers) {
-        int answer = 0;
-        
-        int[] computers_parent = new int[n];
-        
-        for(int i=0; i<n; i++){
-            computers_parent[i] = i+1;
-        }
-        
-        for(int i=0; i<computers.length; i++){
-            int parent = n+1;
-            
-            for(int j=0; j<computers[i].length; j++){
-                if(computers[i][j] == 1){
-                    parent = Math.min(parent, find_parent(j, computers_parent));
-                }
-            }
-            
-            for(int j=0; j<computers[i].length; j++){
-                if(computers[i][j] == 1){
-                    computers_parent[computers_parent[j]-1] = parent;
-                    computers_parent[j] = parent;
-                }
-            }
-            
-            for(int k=n-1; k>=0; k--){
-                computers_parent[k] = find_parent(k, computers_parent);
-            }
-        }
-        
-        System.out.println(Arrays.toString(computers_parent));
-        
-        Set<Integer> set = new HashSet<>();
-        
-        for(int i=0; i<n; i++){
-            set.add(computers_parent[i]);
-        }
-        
-        return set.size();
+        boolean[] visited = new boolean[n];
+        int count = 0;
+        for (int i = 0; i < n; i++)
+            if (!visited[i]) { dfs(i, n, computers, visited); count++; }
+        return count;
     }
-    
-    public static int find_parent(int idx, int[] computers_parent){
-        while(computers_parent[idx] != computers_parent[computers_parent[idx]-1]){
-            idx = computers_parent[idx]-1;
-        }
-        
-        return computers_parent[idx];
+    void dfs(int cur, int n, int[][] computers, boolean[] visited) {
+        visited[cur] = true;
+        for (int next = 0; next < n; next++)
+            if (computers[cur][next] == 1 && !visited[next])
+                dfs(next, n, computers, visited);
     }
 }
